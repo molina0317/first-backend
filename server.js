3f93f3322fc986eef52c2b35dce89ec12273bb0a
molina0317 <molinasanchezluisanselmo@gmail.com>
2025-02-19 21:38:32 -0300

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .catch(err => console.log(err));
 
 app.use(cors({
-  origin: "https://first-frontend-mx5i950xn-tonys-projects-26d88fe1.vercel.app",  // Frontend URL (Vercel)
+  origin: "https://webgrid.djpgroup.co.uk",  // Frontend URL (Vercel)
   credentials: true,  // If you are using cookies or credentials
 }));
 app.use(express.json());
