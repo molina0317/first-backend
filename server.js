8c51110c7d17e8bf2aa9c1695f7670d062d61f76
molina0317 <molinasanchezluisanselmo@gmail.com>
2025-02-19 22:54:34 -0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
-app.use(cors());
+app.use(cors({
+  origin: "https://webgrid.djpgroup.co.uk",  // Frontend URL (Vercel)
+  credentials: true,  // If you are using cookies or credentials
+}));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
